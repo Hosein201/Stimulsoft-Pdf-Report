2536f91a8375f5e7c43166faf18913ce4caa7be1
Hossein Golmohammadi <42300369+Hosein201@users.noreply.github.com>
2023-05-03 14:04:41 +0330

    import multi data

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using Stimulsoft.Report;
+﻿using Stimulsoft.Base;
+using Stimulsoft.Report;
+using Stimulsoft.Report.Components;
 using Stimulsoft.Report.Export;
 
 namespace ConsoleApp2
@@ -20,11 +22,12 @@ namespace ConsoleApp2
             var report = new StiReport();
             report.Load(fs);
 
-            var ti = new Ticket
+
+            var x = new List<Ticket> {new Ticket
             {
                 Destination = "تهران",
                 FullFamily = "حسین گل محمدی",
-                FullPrice = "100.000",
+       
                 Origin = "مشهد",
                 OriginMoveTime = "16:15",
                 WagonNumber = "11",
@@ -37,25 +40,63 @@ namespace ConsoleApp2
                 TicketPrice = "5.912.000",
                 Price = "6.000.000",
                 TicketDate = "1402/02/06",
-                TotalRows = "1",
+            
                 MoveDate = "1402/02/06 چهار شنبه",
                 SecurityNumber = "425198188",
                 TicketSeries = "1",
                 TicketNumber = "635100440"
-            };
-            report.RegData(nameof(Ticket), ti);
+            } ,
+            new Ticket
+            {
+                Destination = "تهران",
+                FullFamily = "حسین گل محمدی2",
+         
+                Origin = "مشهد",
+                OriginMoveTime = "16:15",
+                WagonNumber = "11",
+                SeatNumber = "5",
+                TrainNumber = "197",
+                WagonTypeName = "5 ستاره فدک - سلامت",
+                TimeOfArrivalToDestination = "03:45",
+                TariffName = "تمام-مسافرین عادی",
+                StationPrice = "88.000",
+                TicketPrice = "5.912.000",
+                Price = "6.000.000",
+                TicketDate = "1402/02/06",
+             
+                MoveDate = "1402/02/06 چهار شنبه",
+                SecurityNumber = "425198188",
+                TicketSeries = "1",
+                TicketNumber = "635100440"
+            }};
 
+
+            report.Dictionary.Clear();
+            report.RegData("Ticket",x);
+            report.RegData("TicketFooter", new fo { FullPrice ="100000" , TotalRows ="2"});
+            report.Dictionary.Synchronize();      
+            report.Compile();
             report.Render();
 
 
             var setting = new StiPdfExportSettings
             {
-                ImageQuality = 1.0f,
+                ImageQuality = 2f,
                 ImageResolution = 300,
                 EmbeddedFonts = false,
                 UseUnicode = true,
+                ImageFormat = StiImageFormat.Color,
                 StandardPdfFonts = true,
+                Compressed=true
             };
+
+            //report.NeedsCompiling = false;
+            //report.IsRendered = true;
+
+            //foreach (StiPage page in report.RenderedPages.Items)
+            //{
+            //    report.RenderedPages.Add(page);
+            //}
 
             report.ExportDocument(StiExportFormat.Pdf, "D:\\GitHub\\ConsoleApp2\\Report.pdf", setting);
         }
@@ -77,11 +118,17 @@ namespace ConsoleApp2
         public string TicketPrice { get; set; }
         public string Price { get; set; }
         public string TicketDate { get; set; }
-        public string TotalRows { get; set; }
-        public string FullPrice { get; set; }
+
         public string MoveDate { get; set; }
         public string SecurityNumber { get; set; }
         public string TicketSeries { get; set; }
         public string TicketNumber { get; set; }
     }
+
+     class fo
+    {
+        public string TotalRows { get; set; }
+        public string FullPrice { get; set; }
+    }
+         
 }
